1dde66a91c00fcc13c5b64a15edaa28c42e984c0
YuraT <ytatisch@gmail.com>
2016-03-03 19:34:46 -0800

    ChangeX and ChangeY functions

--- public_html/js/sprite.js
+++ public_html/js/sprite.js
@@ -6,9 +6,31 @@
 
 function Sprite(images, sounds, text) {
     this.scripts = {};
-    this.images = images;
+    this.images = [];
+    this.registerImages(images);
     this.sounds = sounds;
     this.text = text;
+    this.x = 0;
+    this.y = 0;
 }
 
+Sprite.prototype.changeX = function(units) {
+    this.x += units;
+    this.images[0].style.left = this.x + "px";
+};
 
+Sprite.prototype.changeY = function(units) {
+    this.y += units;
+    this.images[0].style.top = this.y + "px";
+};
+
+
+Sprite.prototype.registerImages = function(imageNames) {
+    for (var i = 0; i<imageNames.length; i++) {
+        var img = document.createElement('img');
+        img.src = imageNames[i];
+        img.style.position = 'absolute';
+        $('#main').append(img);
+        this.images.push(img);
+    }
+};
